a8b23529f586a84a4858a59222f17945d914ad03
Eduardo Nascimento <137827827+EARNascimento@users.noreply.github.com>
2026-06-05 23:40:34 +0000

    feat: Adicionando os métodos do arquivo igdb.js, criando um GET para realizar a busca de um jogo

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-
+const { initIGDB, searchGames } = require('../services/igdb');
 //Criando um banco de dados falso, verificar se vamos utilizar uma base dados para buscar esses dados.
 let games = [
     {id: 1, title: "test", platform: "pc", status: "Added", rating: 5, review: "A great game"}
 ];
 
+initIGDB();
+
 //Rotas
 //READ
+
+router.get("/search/:gameName", async(req, res) => {
+    try{
+        const { gameName } = req.params;
+        const results = await searchGames(gameName);
+
+        res.json(results);
+    } catch(error){
+        res.status(500).json({ erro: 'Erro ao buscar jogo', detalhe: error.message});
+    }
+});
+
 app.get('/', (req, res) => {
     console.log(games); 
 });
 
 //CREATE
-app.post()
+router.post("/", (req, res) => {
+    res.status(201).json({ mensagem: "Rota POST pronta, lógica a implementar" });
+});
+
 //Exportando o roteador
 module.exports = router;
