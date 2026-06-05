9d6666288b482e54c6993fe144e7ba1012f24b16
Eduardo Nascimento <137827827+EARNascimento@users.noreply.github.com>
2026-06-05 23:42:06 +0000

    Corrigindo alguns imports, não me atentei que o tipo do projeto estava como commonjs, criando uma lógica para o servidor subir apenas se a API do IGDB conseguir ser consultada

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const { fileURLToPath } = require('url');
+const { initIGDB } = require('./services/igdb.js');
+const gamesRouter = require('./routes/games.js');
 
 //Camuflando a porta com o 'dotenv'.
 const PORT = process.env.PORT;
@@ -9,7 +12,12 @@ const PORT = process.env.PORT;
 //Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); //Agora qualquer arquivo no diretório `public` pode ser acessado.
+app.use('/games', gamesRouter);
 
-app.listen(PORT, () => {
+initIGDB().then(() => {
+    app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}!`)
+    });
+}).catch(erro => {
+    console.error("Falha fatal: não foi possível obter o token do IGDB.", erro)
 });
