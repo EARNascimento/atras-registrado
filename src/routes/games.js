070424aed5340037b3af859c94f3131786ff2058
Eduardo Nascimento <137827827+EARNascimento@users.noreply.github.com>
2026-06-06 13:43:26 +0000

    feat: Criando as rotas para adicionar um jogo e deletar um jogo

--- src/routes/games.js
+++ src/routes/games.js
@@ -3,15 +3,12 @@ const app = express();
 const router = express.Router();
 const { initIGDB, searchGames } = require('../services/igdb');
 //Criando um banco de dados falso, verificar se vamos utilizar uma base dados para buscar esses dados.
-let games = [
-    {id: 1, title: "test", platform: "pc", status: "Added", rating: 5, review: "A great game"}
-];
+let games = [];
 
-initIGDB();
+//initIGDB();
 
 //Rotas
-//READ
-
+//Busca os jogos no IGDB
 router.get("/search/:gameName", async(req, res) => {
     try{
         const { gameName } = req.params;
@@ -23,13 +20,46 @@ router.get("/search/:gameName", async(req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
+// Lista todos os jogos adicionados
+router.get('/', (req, res) => {
     console.log(games); 
 });
 
-//CREATE
+//Adiciona um jogo (Máx. 10)
 router.post("/", (req, res) => {
-    res.status(201).json({ mensagem: "Rota POST pronta, lógica a implementar" });
+
+    if (!req.body) {
+        return res.status(400).json({
+            error: "O corpo da requisição não foi enviado."
+        });
+    }
+
+    if (games.lenght >= 10) {
+        return res.status(400).json({
+            error : "Limite de 10 jogos atingido!"
+        });
+    }
+
+    let URLdaCapa = '';
+    if (req.body.cover && req.body.cover.url){
+        URLdaCapa = `https:${req.body.cover.url}`;
+    }
+
+    const novoJogo = {
+        id: req.body.id,
+        title: req.body.name,
+        coverUrl: URLdaCapa,
+        rating: req.body.rating || 0
+    };
+
+    games.push(novoJogo);
+    res.status(201).json(novoJogo);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    games = games.filter(g => g.id != id);
+    res.json({ message: "Jogo removido com sucesso"});
 });
 
 //Exportando o roteador
